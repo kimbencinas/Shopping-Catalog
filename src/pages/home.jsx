d9249117c853f56dc8b663031e0e9fd2a6d8df99
Kimberly Encinas <kimbencinas@gmail.com>
2025-05-20 17:24:40 -0700

    Change background of homepage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -24,17 +24,19 @@ function Home() {
     <>
         <div>
             <Navbar />  
-            <h1 className="font-semibold text-2xl text-center mt-8">Shopping Catalog</h1>
-            <div className="card-container md:flex justify-center xs:flex xs:flex-col mt-4">
-                {products.map(product => (
-                    <ProductCard
-                        key={product.id}
-                        id={product.id}
-                        name={product.name}
-                        image={product.image}
-                        price={product.price}
-                    />
-                ))}
+            <div className="bg-gray-100 min-h-screen">
+                <h1 className="font-semibold text-2xl text-center pt-12">Shopping Catalog</h1>
+                <div className="card-container md:flex justify-center xs:flex xs:flex-col mt-4">
+                    {products.map(product => (
+                        <ProductCard
+                            key={product.id}
+                            id={product.id}
+                            name={product.name}
+                            image={product.image}
+                            price={product.price}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
     </>
